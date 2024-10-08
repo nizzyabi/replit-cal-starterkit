02def3d2ce83df78cbed323faac631f1a62162e5
NizzyABI <nizabizaher@gmail.com>
2024-10-08 13:03:15 -0500

    hi

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,11 +6,6 @@ const devDb = new PrismaClient();
 async function main() {
   for (const filterOption of filterOptions) {
     console.log(`attempting to upsert ${filterOption.fieldId}`);
-    await devDb.filterOption.upsert({
-      where: { fieldId: filterOption.fieldId },
-      create: filterOption,
-      update: filterOption,
-    });
     console.log(`✅ {filterOption.fieldId} upserted`);
   }
 }
